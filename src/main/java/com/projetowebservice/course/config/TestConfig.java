620dc42f4b5572ecb481ffa154ab6d4a8caca55b
Victor Mesquita <luizvictor2513@gmail.com>
2025-04-14 12:55:37 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/projetowebservice/course/config/TestConfig.java
+++ src/main/java/com/projetowebservice/course/config/TestConfig.java
@@ -1,6 +1,5 @@
 package com.projetowebservice.course.config;
 
-import java.sql.Array;
 import java.time.Instant;
 import java.util.Arrays;
 
@@ -11,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.projetowebservice.course.entities.Category;
 import com.projetowebservice.course.entities.Order;
+import com.projetowebservice.course.entities.OrderItem;
 import com.projetowebservice.course.entities.Product;
 import com.projetowebservice.course.entities.User;
 import com.projetowebservice.course.entities.enums.OrderStatus;
 import com.projetowebservice.course.repositories.CategoryRepository;
+import com.projetowebservice.course.repositories.OrderItemRepository;
 import com.projetowebservice.course.repositories.OrderRepository;
 import com.projetowebservice.course.repositories.ProductRepository;
 import com.projetowebservice.course.repositories.UserRepository;
@@ -34,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private ProductRepository productRepository;
 
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -67,6 +71,14 @@ public class TestConfig implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
 	}
 	
 }
