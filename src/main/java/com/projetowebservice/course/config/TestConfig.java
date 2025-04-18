17cd0b8afeaed50250459ca66b68fab9a84d5853
Victor Mesquita <luizvictor2513@gmail.com>
2025-04-18 13:52:25 -0300

    Payment, one-to-one association

--- src/main/java/com/projetowebservice/course/config/TestConfig.java
+++ src/main/java/com/projetowebservice/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projetowebservice.course.entities.Category;
 import com.projetowebservice.course.entities.Order;
 import com.projetowebservice.course.entities.OrderItem;
+import com.projetowebservice.course.entities.Payment;
 import com.projetowebservice.course.entities.Product;
 import com.projetowebservice.course.entities.User;
 import com.projetowebservice.course.entities.enums.OrderStatus;
@@ -79,6 +80,9 @@ public class TestConfig implements CommandLineRunner {
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2025-04-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
-	
 }
