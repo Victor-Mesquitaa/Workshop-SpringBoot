3c95084314fb3590359a2b513f73ed4c20f9af1f
Victor Mesquita <luizvictor2513@gmail.com>
2025-04-24 12:48:22 -0300

    Exception handling - delete

--- src/main/java/com/projetowebservice/course/services/OrderService.java
+++ src/main/java/com/projetowebservice/course/services/OrderService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.projetowebservice.course.entities.Order;
-import com.projetowebservice.course.entities.User;
 import com.projetowebservice.course.repositories.OrderRepository;
 
 
