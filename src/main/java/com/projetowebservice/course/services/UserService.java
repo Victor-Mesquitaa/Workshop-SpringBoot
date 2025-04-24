2398fe0ece8626d4ea3d7a3cd1dd918d98cd6c23
Victor Mesquita <luizvictor2513@gmail.com>
2025-04-24 12:55:34 -0300

    Exception handling - update

--- src/main/java/com/projetowebservice/course/services/UserService.java
+++ src/main/java/com/projetowebservice/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.projetowebservice.course.repositories.UserRepository;
 import com.projetowebservice.course.services.exceptions.DatabaseException;
 import com.projetowebservice.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 
 @Service
 public class UserService {
@@ -44,9 +46,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	//Atualizar usuário
